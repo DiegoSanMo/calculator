f7f5b889f9316e2bb7a91f3234891218386f41f7
Diego Mosan <diego14290764@itcg.edu.mx>
2018-11-12 20:55:33 -0600

    First commit

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -7,8 +7,31 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
+  expresion: any = '';
+  btn:any;
+  cantOperatos: number = 0;
   constructor(public navCtrl: NavController) {
+    
+  }
 
+  getValueButton(event:any){
+
+    let val = event.toElement.textContent
+    if(val === "DEL"){
+      this.expresion = '';
+      console.log('Borrar');
+      
+    } else if(val === "="){
+     this.expresion =  eval(this.expresion);
+      
+    } else {
+      val == "/" || val == "+" || val == "*" || val == "-" ? this.cantOperatos++ : this.cantOperatos = 0;
+      if(this.cantOperatos > 1){
+
+      } else {
+        this.expresion += val;
+      }
+    }
   }
 
 }
